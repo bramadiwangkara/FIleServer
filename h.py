16ecbac343feddc50ba445e217a4ebfb04c2bf3f
Unknown <naufalpf@gmail.com>
2017-12-15 17:07:06 +0700

    Memperbaiki akses luar dalam 1 lan

--- h.py
+++ h.py
@@ -14,14 +14,14 @@ import subprocess
 #inisialisasi
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-PORT = 11000
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-httpd.serve_forever()
+#PORT = 11000
+#Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+#httpd = SocketServer.TCPServer(("", PORT), Handler)
+#httpd.serve_forever()
 
 #proses binding
-#address = '10.151.253.51'
-#server_address = (address, 11000)
+address = '10.151.253.51'
+server_address = (address, 11000)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
